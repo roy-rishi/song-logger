b27f82c1b363fb8227f5b50485c7763452d09ddf
Rishi <72419270+roy-rishi@users.noreply.github.com>
2023-06-10 12:46:42 -0700

    init auth

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import cred
+
+
+def raiseNetworkError():
+    raise Exception("\033[91mERROR: Could not connect to Spotify. Device may be offline, or the network may be blocking this request")
+
+def nowPlaying():
+    nowPlaying = sp.current_playback()
+    print(nowPlaying)
+
+
+scope = "user-read-playback-state"
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=cred.client_id, client_secret= cred.client_secret, redirect_uri=cred.redirect_url, scope=scope))
+
+nowPlaying()
